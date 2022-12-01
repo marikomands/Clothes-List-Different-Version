f7c2ff496031fc0b49d16c2608028a2a846a387d
marikomands <marikomands@gmail.com>
2022-12-01 15:00:28 +0900

    created App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CategoryList from "./CategoryList";
+import TypeList from "./TypeList";
+import ItemList from "./ItemList";
+
+const App = () => {
+  return (
+    <div>
+      <CategoryList />
+      <TypeList />
+      <ItemList />
+    </div>
+  );
+};
+
+export default App;
