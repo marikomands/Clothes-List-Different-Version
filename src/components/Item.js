75ba355ca8a57987471ca879bc3f173f518f95b8
marikomands <marikomands@gmail.com>
2022-12-02 14:06:37 +0900

    imported App.css

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
 
 const ClothesItem = ({ item }) => {
   const [imageSrc, setImageSrc] = useState(null);
